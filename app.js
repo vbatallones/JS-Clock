43d43a17ed4c6e4e27258d9333cdc333a3731fbb
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-10-15 20:21:07 -0700

    [app.js] add functionalities for each hand

---
+++ app.js
@@ -0,0 +1,18 @@
+const secondHand = document.querySelector('.second-hand')
+const minuteHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hour = now.getHours();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    console.log(seconds);
+}
+
+setInterval(setDate, 1000);
